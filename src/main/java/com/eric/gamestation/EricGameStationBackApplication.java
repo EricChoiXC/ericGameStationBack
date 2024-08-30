914ccac92e0dd6c9e49b5276b64f91cc60ceff5d
蔡咏钦 <852426401@.qq.com>
2024-08-30 16:21:38 +0800

    eric: init program

---
+++ src/main/java/com/eric/gamestation/EricGameStationBackApplication.java
@@ -0,0 +1,13 @@
+package com.eric.gamestation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EricGameStationBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EricGameStationBackApplication.class, args);
+    }
+
+}
